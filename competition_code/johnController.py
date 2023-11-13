fb6c88b85e9072241f8fe63e54f5608509edc6e2
Audrey Han <audreyhan222@gmail.com>
2023-11-12 21:45:59 -0800

    not working occupancy map

--- competition_code/johnController.py
+++ competition_code/johnController.py
@@ -73,6 +73,8 @@ async def main():
         image_width=500,
         image_height=250
     )
+
+    occ_map_producer = roar_py_interface.RoarPyOccupancyMapProducer(carla_world.maneuverable_waypoints, 200, 200, 5, 5)
     integral_error = 0
     start_time = time.time()
     assert camera is not None
@@ -93,6 +95,8 @@ async def main():
             location_data = await locaton.receive_observation()
 
             checkpoint_display.update_checkpoints(location_data.x, location_data.y)
+
+            occupancy_map = occ_map_producer.plot_occupancy_map(vehicle.get_3d_location()[:2], vehicle.get_roll_pitch_yaw()[2])
             # Find the waypoint closest to the vehicle
             current_waypoint_idx = filter_waypoints(
                 vehicle_location,
@@ -128,7 +132,7 @@ async def main():
             logging.info("ITERATION TIME: " + str(iteration_time))
             logging.info("TOTAL KI VAL: " + str(Ki * integral_error))
             logging.info("Thorttle Control: " + str(throttle_control))
-            render_ret = viewer.render(camera_data, depth_camera_data, location_data, way_points, target_speed, current_speed, graphnum)
+            render_ret = viewer.render(camera_data, depth_camera_data, occupancy_map, location_data, way_points, target_speed, current_speed, graphnum)
             # If user clicked the close button, render_ret will be None
             if render_ret is None:
                 break
